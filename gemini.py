80b1c6b0c4cbb36f4e4c2222247aff1191610ec3
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-06-11 22:20:14 -0500

    Actualizar clave API en gemini.py a un valor seguro

--- gemini.py
+++ gemini.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBmf_0dxaf-Y3w-UK6vcZ55L_G1fKZwjFU")
+genai.configure(api_key=".")
 models = genai.list_models()
 
 for m in models:
